ae7394957014131d1ab6407f7c00382355f3848e
aliaksei_dziashko <aliaksei_dziashko@epam.com>
2013-11-22 12:36:03 +0300

    v4 update, refactor

--- Let'sWatch!/src/com/sickfuture/letswatch/app/fragment/common/PagerFragment.java
+++ Let'sWatch!/src/com/sickfuture/letswatch/app/fragment/common/PagerFragment.java
@@ -1,46 +1,37 @@
 package com.sickfuture.letswatch.app.fragment.common;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.astuetz.viewpager.extensions.PagerSlidingTabStrip;
-import com.sickfuture.letswatch.R;
+//import com.astuetz.viewpager.extensions.PagerSlidingTabStrip;
 
 public abstract class PagerFragment extends Fragment {
 
-	private PagerSlidingTabStrip mTabs;
-	private FragmentPagerAdapter mSectionsPagerAdapter;
-	private ViewPager mViewPager;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		mSectionsPagerAdapter = getPagerAdapter();
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_pager, null);
-		mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
-		mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-		final int pageMargin = (int) TypedValue.applyDimension(
-				TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
-						.getDisplayMetrics());
-		mViewPager.setPageMargin(pageMargin);
-
-		mTabs.setViewPager(mViewPager);
-
-		return view;
-	}
-
-	protected abstract FragmentPagerAdapter getPagerAdapter();
+//	private PagerSlidingTabStrip mTabs;
+//	private FragmentPagerAdapter mSectionsPagerAdapter;
+//	private ViewPager mViewPager;
+//
+//	@Override
+//	public void onCreate(Bundle savedInstanceState) {
+//		mSectionsPagerAdapter = getPagerAdapter();
+//		super.onCreate(savedInstanceState);
+//	}
+//
+//	@Override
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//			Bundle savedInstanceState) {
+//		View view = inflater.inflate(R.layout.fragment_pager, null);
+//		mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
+//		mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+//		mViewPager.setAdapter(mSectionsPagerAdapter);
+//		final int pageMargin = (int) TypedValue.applyDimension(
+//				TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+//						.getDisplayMetrics());
+//		mViewPager.setPageMargin(pageMargin);
+//
+//		mTabs.setViewPager(mViewPager);
+//
+//		return view;
+//	}
+//
+//	protected abstract FragmentPagerAdapter getPagerAdapter();
 
 }
--- Let'sWatch!/src/com/sickfuture/letswatch/app/fragment/pager/TheatersPagerFragment.java
+++ Let'sWatch!/src/com/sickfuture/letswatch/app/fragment/pager/TheatersPagerFragment.java
@@ -1,31 +1,24 @@
 package com.sickfuture.letswatch.app.fragment.pager;
 
-import android.support.v4.app.FragmentPagerAdapter;
-
-import com.sickfuture.letswatch.R;
-import com.sickfuture.letswatch.adapter.SectionsPagerAdapter;
 import com.sickfuture.letswatch.app.fragment.common.PagerFragment;
-import com.sickfuture.letswatch.app.fragment.theaters.BoxOfficeFragment;
-import com.sickfuture.letswatch.app.fragment.theaters.OpeningFragment;
-import com.sickfuture.letswatch.app.fragment.theaters.TheatersFragment;
-import com.sickfuture.letswatch.app.fragment.theaters.UpcomingFragment;
 
+@Deprecated
 public class TheatersPagerFragment extends PagerFragment {
 
-	private static final String LOG_TAG = TheatersPagerFragment.class
-			.getSimpleName();
-
-	@Override
-	protected FragmentPagerAdapter getPagerAdapter() {
-		String[] sFragments = new String[] { BoxOfficeFragment.class.getName(),
-				UpcomingFragment.class.getName(),
-				TheatersFragment.class.getName(),
-				OpeningFragment.class.getName() };
-		String[] sTabTitles = getResources().getStringArray(
-				R.array.titles_theater);
-
-		return new SectionsPagerAdapter(getActivity(),
-				getChildFragmentManager(), sFragments, sTabTitles);
-	}
+//	private static final String LOG_TAG = TheatersPagerFragment.class
+//			.getSimpleName();
+//
+//	@Override
+//	protected FragmentPagerAdapter getPagerAdapter() {
+//		String[] sFragments = new String[] { BoxOfficeFragment.class.getName(),
+//				UpcomingFragment.class.getName(),
+//				TheatersFragment.class.getName(),
+//				OpeningFragment.class.getName() };
+//		String[] sTabTitles = getResources().getStringArray(
+//				R.array.titles_theater);
+//
+//		return new SectionsPagerAdapter(getActivity(),
+//				getChildFragmentManager(), sFragments, sTabTitles);
+//	}
 
 }
